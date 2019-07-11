81f27bc044d2e6c89091e449d75e9ee934998899
cyt <470922684@qq.com>
2019-07-11 20:19:17 +0800

    git第一次尝试

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        首页
+    </div>
+</template>
+
+<script>
+import Header from "@/components/header"
+export default {
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
